d9f749bd3ba8f0cebf1f0ba0c368db45632bf97b
Nathan Borror <nathan@nathanborror.com>
2025-10-28 14:35:14 -0700

    Initial package

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "INI",
+    platforms: [
+        .iOS(.v18),
+        .macOS(.v15),
+    ],
+    products: [
+        .library(name: "INI", targets: ["INI"])
+    ],
+    targets: [
+        .target(name: "INI"),
+        .testTarget(name: "INITests", dependencies: ["INI"]),
+    ]
+)
